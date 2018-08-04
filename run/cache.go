666009f02311c3246969831197809eca1ef4aeff
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-08-04 18:55:23 +0200

    Squashed 'extra/dargs/' content from commit bdedbf7
    git-subtree-dir: extra/dargs
    git-subtree-split: bdedbf7ad3eeb45f35e7bfd8391345a9d1285ac2

---
+++ run/cache.go
@@ -0,0 +1,37 @@
+package run
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+
+	cache "github.com/patrickmn/go-cache"
+)
+
+func loadCache(f string) (*cache.Cache, error) {
+	if _, err := os.Stat(f); os.IsNotExist(err) {
+		return cache.New(1*time.Nanosecond, 10*time.Minute), nil
+	}
+	d, err := ioutil.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	var out map[string]cache.Item
+	err = yaml.Unmarshal(d, &out)
+	c := cache.NewFrom(5*time.Minute, 10*time.Minute, out)
+	return c, nil
+}
+
+func saveCache(c *cache.Cache, f string) error {
+	if err := os.MkdirAll(path.Dir(f), 0755); err != nil {
+		return err
+	}
+	d, err := yaml.Marshal(c.Items())
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(f, d, 0644)
+}
